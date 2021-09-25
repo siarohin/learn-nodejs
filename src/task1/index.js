1e4deb95a5f4b99a8f885d8c4614aa9fb029a2bc
Aliaksandr Siarohin <Aliaksandr_Siarohin@epam.com>
2021-09-25 15:42:33 +0300

    feat: reverse string on user input

---
+++ src/task1/index.js
@@ -0,0 +1,12 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdout.write('Push the value:' + '\n\n');
+process.stdin.on('data', reverseString);
+
+function reverseString(value) {
+  let result = '';
+  for (let current of value) {
+    result = current + result;
+  }
+  process.stdout.write(result.replace(/\r?\n|\r/g, '') + '\n\n');
+}
